575dbc44debef27d8241fc008ba3800000c7999d
410 <58301450+410-dev@users.noreply.github.com>
2020-06-12 02:04:01 +0900

    Added frontend xcode projects

---
+++ HARTS Student/HARTS Student/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HARTS Student
+//
+//  Created by Hoyoun Song on 2020/06/12.
+//  Copyright © 2020 Hoyoun Song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
---
+++ HARTS Teacher/HARTS Teacher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HARTS Teacher
+//
+//  Created by Hoyoun Song on 2020/06/12.
+//  Copyright © 2020 Hoyoun Song. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
