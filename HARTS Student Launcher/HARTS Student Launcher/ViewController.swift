98585dbf312c4ca804866728407da7c83cefce1d
410 <58301450+410-dev@users.noreply.github.com>
2020-06-13 01:14:00 +0900

    Launcher added

---
+++ HARTS Student Launcher/HARTS Student Launcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HARTS Student Launcher
+//
+//  Created by Hoyoun Song on 2020/06/13.
+//  Copyright © 2020 Hoyoun Song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
