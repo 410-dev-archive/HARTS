3c3062e7d1ca9e618b51dff9b0dea969b57778c7
410 <58301450+410-dev@users.noreply.github.com>
2020-06-13 13:18:45 +0900

    Change of algorithm: No async need

--- backend-student/locker.sh
+++ backend-student/locker.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-while [[ -f "/tmp/TESTDONE.harts" ]]; do
-        killall Dock
-        killall Finder
-        killall SystemUIServer
-        killall QuickLookUIService
-        killall Siri
-        killall Spotlight
-        killall USBAgent
-        killall SidecarRelay
-        killall sharingd
-        killall AirPlayUIAgent
-        killall ExternalQuickLookSatellite
-	killall TouchBarServer
-done
+killall Dock
+killall Finder
+killall SystemUIServer
+killall QuickLookUIService
+killall Siri
+killall Spotlight
+killall SidecarRelay
+killall sharingd
+killall AirPlayUIAgent
+killall ExternalQuickLookSatellite
