5fa1d327452ed48c3b9a6302243945e1e26efac7
410 <58301450+410-dev@users.noreply.github.com>
2020-06-26 19:48:38 +0900

    OrtaOS - Program supporting OS

---
+++ backend/student/unix/security/OrtaOS/boot/osstart.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export NULLVAR="null"
+mkdir -p "$PWD/cache/SIG"
+echo "[*] Running initiation script..."
+"$PWD/System/sbin/initsc"
+if [[ -f "$PWD/cache/upgraded" ]]; then
+	echo "System is upgraded. Stopping boot process."
+	exit 9
+fi
+cd "$PWD/cache/def"
+for file in *.def
+do
+	source "$file"
+done
+cd "$VFS"
+echo "[*] Running frameworks asyncronously..."
+mkdir -p "$CACHE/frameworks"
+"$SYSTEM/sbin/frameworks" --load
+if [[ -f "$CACHE/update-complete" ]]; then
+	echo "[*] Update complete."
+	rm -f "$CACHE/update-complete"
+elif [[ -f "$CACHE/load-failed" ]]; then
+	rm -f "$CACHE/load-failed"
+	echo "[!] Framworks loader returned non-zero exit code."
+	echo "[!] Unable to continue!"
+	while [[ true ]]; do
+		sleep 1000
+	done
+else
+	echo "[O] Frameworks loaded without problem."
+	echo "[*] Starting Interface..."
+fi
