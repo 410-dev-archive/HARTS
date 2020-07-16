9440d7fe0d843e1955045c683cb34832fcd7cce2
410 <58301450+410-dev@users.noreply.github.com>
2020-07-16 22:51:50 +0900

    아 ㅅㅂ ㅈ될뻔 했네
    Restored from 88ae5fb9ce

--- backend/student/unix/lockvf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while [[ ! -f "/tmp/TESTDONE.harts" ]]; do
-        sleep 1
-        export dtcfndr=$(ps -ax | grep "/System/Library/CoreServices/[F]inder")
-        if [[ -z "$dtcfndr" ]]; then
-                touch "/tmp/FINDER_STARTED.harts"
-        fi
-done
