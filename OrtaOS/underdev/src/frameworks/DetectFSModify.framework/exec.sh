64703963e8d925dae0652e4381d0f7d61b8c992d
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 19:03:56 +0900

    Only runs if library exists

--- OrtaOS/underdev/src/frameworks/DetectFSModify.framework/exec.sh
+++ OrtaOS/underdev/src/frameworks/DetectFSModify.framework/exec.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-"$ORTA/fswatch" --one-event -L -r "$SYSTEM"
-echo "terminate" > "$TTYIN"
+if [[ -e "/usr/local/Cellar/fswatch/1.14.0/lib/libfswatch.11.dylib" ]]; then
+	"$ORTA/fswatch" --one-event -L -r "$SYSTEM"
+	echo "terminate" > "$TTYIN"
+fi
