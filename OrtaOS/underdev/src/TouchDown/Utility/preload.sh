3285b5f3616e4cb4229666ff06ac6318bfa147ad
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 17:23:12 +0900

    Removed loginscreen loader code (made dummy)

--- OrtaOS/underdev/src/TouchDown/Utility/preload.sh
+++ OrtaOS/underdev/src/TouchDown/Utility/preload.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-if [[ ! -f "$TDLIB/Utility/$(mplxr USER/INTERFACE/START_MODE)" ]]; then
-	echo "[*] Unable to locate preload utility."
-	while [[ true ]]; do
-		sleep 1000
-	done
-else
-	"$TDLIB/Utility/$(mplxr USER/INTERFACE/START_MODE)"
-fi
+exit 0
