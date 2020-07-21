87c34a6efeb7465f4b2618bb7cdcf3c8a8913db3
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 17:09:23 +0900

    Little modified library deployer

---
+++ OrtaOS/underdev/src/init/orta-plugin/ExtractFSWatchdylib.init/exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ ! -d "/usr/local/Cellar/fswatch/lib" ]]; then
+	if [[ ! -d "/usr/local/Cellar" ]]; then
+		echo "$(whoami)" > /tmp/uname
+		osascript -e 'do shell script "mkdir -p /usr/local/Cellar; chown -R $(</tmp/uname) /usr/local/Cellar" with prompt "Runtime Extraction" with administrator privileges'
+	fi
+	mkdir -p "/usr/local/Cellar/fswatch"
+	unzip -q "$1/fswatch.zip" -d "/usr/local/Cellar/fswatch"
+	touch "/usr/local/Cellar/fswatchbyharts"
+fi
