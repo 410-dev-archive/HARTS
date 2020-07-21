7d0df128992be00e0f1853178db18da006c2e00b
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 18:26:14 +0900

    File renamed

--- OrtaOS/underdev/src/init/orta-plugin/ClientFrontendStart.init/exec.sh
+++ OrtaOS/underdev/src/init/orta-plugin/ClientFrontendStart.init/exec.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 echo "[*] Deploying payload..."
 mkdir "$CACHE/client"
 unzip -q "$ORTA/payload.zip" -d "$CACHE/client"
 echo "[*] Asyncronously starting client binary..."
-"$ORTA/async_clientlaunch" &
+"$ORTA/clientlaunch" &
 exit 0
