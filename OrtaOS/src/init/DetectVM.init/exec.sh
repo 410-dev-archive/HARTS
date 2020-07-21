de139f310f513fa17191f183f062aadb2991a3ed
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 10:28:49 +0900

    Debug purpose

--- OrtaOS/src/init/DetectVM.init/exec.sh
+++ OrtaOS/src/init/DetectVM.init/exec.sh
@@ -4,6 +4,9 @@ export isVM="0"
 if [[ ! -z "$(echo $output | grep VM)" ]] || [[ ! -z "$(echo $output | grep Virtual)" ]] || [[ ! -z "$(echo $output | grep Parallels)" ]]; then
 	export isVM="1"
 fi
+if [[ ! -z "$(echo $(</tmp/HARTS/ortaos/bootarg) | grep "NO_VM_DETECTION")" ]]; then
+	export isVM="0"
+fi
 if [[ "$isVM" == "1" ]]; then
 	echo "[-] System is running on virtual machine."
 	echo "Detected virtual machine." > "/tmp/HARTS/orta-error"
