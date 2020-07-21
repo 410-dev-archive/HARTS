3aa6160fa18df4fec43feaccccd9ff025204a62a
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 17:35:50 +0900

    Update init.sh

--- OrtaOS/underdev/src/boot/init.sh
+++ OrtaOS/underdev/src/boot/init.sh
@@ -3,45 +3,12 @@
 function beginningOfSystem() {
 	source "$(dirname "$0")/PLT"
 	source "$(dirname "$0")/ASSIGN"
-	if [[ ! -z "$(echo $b_arg | grep "reset_nvram")" ]]; then
-		rm -rf "$NVRAM"
-		cp -r "$TDLIB/defaults/nvram" "$LIB"
-	elif [[ ! -d "$NVRAM" ]]; then
-		cp -r "$TDLIB/defaults/nvram" "$LIB"
-	fi
-	b_arg="$(<$NVRAM/boot_argument) $b_arg"
+	b_arg="$(<$BOOTARGS) $b_arg"
 	"$SYSTEM/boot/splasher"
 	"$SYSTEM/boot/osstart"
-	if [[ -f "$CACHE/upgraded" ]]; then
-		rm -rf "$CACHE/"*
-		if [[ ! -z "$(echo $b_arg | grep "bootergen2")" ]]; then
-			echo "[*] Leaving..."
-		else
-			hdiutil detach "$CACHE" -force >/dev/null
-			hdiutil detach "$DATA" -force >/dev/null
-			hdiutil detach "$SYSTEM" -force >/dev/null; exit 0
-		fi
-	elif [[ -f "$CACHE/init-load-failed" ]] || [[ -f "$CACHE/framework-load-failed" ]]; then
-		if [[ -f "$ROOTFS/Recovery/boot/init" ]]; then
-			"$ROOTFS/Recovery/boot/init" "$ROOTFS"
-		elif [[ -f "$SYSTEM/TouchDown/Library/Recovery/recovery.dmg" ]]; then
-			mkdir -p "$ROOTFS/Recovery"
-			hdiutil attach "$SYSTEM/TouchDown/Library/Recovery/recovery.dmg" -mountpoint "$ROOTFS/Recovery" >/dev/null
-			"$ROOTFS/Recovery/boot/init" "$ROOTFS"
-		elif [[ -f "$SYSTEM/TouchDown/Library/Recovery/lightrecovery.dmg" ]]; then
-			mkdir -p "$ROOTFS/Recovery"
-			hdiutil attach "$SYSTEM/TouchDown/Library/Recovery/lightrecovery.dmg" -mountpoint "$ROOTFS/Recovery" >/dev/null
-			"$ROOTFS/Recovery/boot/init" "$ROOTFS"
-		fi
-		echo "[*] Stopping boot process..."
-		rm -rf "$CACHE/"*
-		if [[ ! -z "$(echo $b_arg | grep "bootergen2")" ]]; then
-			echo "[*] Leaving..."
-		else
-			hdiutil detach "$CACHE" -force >/dev/null
-			hdiutil detach "$DATA" -force >/dev/null
-			hdiutil detach "$SYSTEM" -force >/dev/null; exit 0
-		fi
+	if [[ -f "$BOOTREFUSE" ]]; then
+		rm -rf "$CACHE"
+		EOS
 	fi
 	cd "$CACHE/def"
 	for file in *.def
@@ -50,63 +17,33 @@ function beginningOfSystem() {
 	done
 }
 
-function pseudoEndOfSystem(){
-	if [[ ! -z "$(echo $b_arg | grep "verbose")" ]]; then
-		echo "[*] Cleaning up frameworks cache..."
-		rm -rf "$CACHE/Frameworks"
-		echo "[*] Cleaning up signal cache..."
-		rm -rf "$CACHE/SIG"
-		echo "[*] Full-flushing cache..."
-		rm -rf "$CACHE/" 2>/dev/null
-		echo "[*] Closing..."
-	else
-		rm -rf "$CACHE/Frameworks"
-		rm -rf "$CACHE/SIG"
-		rm -rf "$CACHE/" 2>/dev/null
-	fi
+function EOS(){
+	echo "[*] Terminating background frameworks..."
+	echo "[*] Loading lists of alive frameworks..."
+	ALIVE=$(ps -ax | grep "$SYSTEM/frameworks[/]")
+	echo "[*] Currently $(echo "$ALIVE" | wc -l) frameworks are up and running."
+	echo "[*] Killing asyncronously..."
+	echo "$ALIVE" | while read proc
+	do
+		frpid=($proc)
+		kill -9 ${frpid[0]}
+		echo "[*] Killed PID: ${frpid[0]}"
+	done
+	echo "[*] Frameworks are closed."
+	echo "[*] Cleaning up frameworks cache..."
+	rm -rf "$CACHE/Frameworks"
+	echo "[*] Cleaning up signal cache..."
+	rm -rf "$CACHE/SIG"
+	echo "[*] Full-flushing cache..."
+	rm -rf "$CACHE/" 2>/dev/null
+	echo "[*] Closing..."
+	hdiutil detach "$SYSTEM" -force >/dev/null; exit 0
 }
 
-function realEndOfSystem(){
-	if [[ ! -z "$(echo $b_arg | grep "verbose")" ]]; then
-		echo "[*] Cleaning up frameworks cache..."
-		rm -rf "$CACHE/Frameworks"
-		echo "[*] Cleaning up signal cache..."
-		rm -rf "$CACHE/SIG"
-		echo "[*] Full-flushing cache..."
-		rm -rf "$CACHE/" 2>/dev/null
-		echo "[*] Closing..."
-		if [[ ! -z "$(echo $b_arg | grep "bootergen2")" ]]; then
-			echo "[*] Leaving..."
-		else
-			hdiutil detach "$CACHE" -force >/dev/null
-			hdiutil detach "$SYSTEM" -force >/dev/null; exit 0
-		fi
-	else
-		rm -rf "$CACHE/Frameworks"
-		rm -rf "$CACHE/SIG"
-		rm -rf "$CACHE/" 2>/dev/null
-		if [[ ! -z "$(echo $b_arg | grep "bootergen2")" ]]; then
-			echo "[*] Leaving..."
-		else
-			hdiutil detach "$CACHE" -force >/dev/null
-			hdiutil detach "$SYSTEM" -force >/dev/null; exit 0
-		fi
-	fi
-}
-
+export -f EOS
 export b_arg="verbose enforce_cli $1 $2 $3"
 
-while [[ true ]]; do
-	beginningOfSystem
-	cd "$ROOTFS"
-	"$SYSTEM/sbin/interface"
-	if [[ -f "$CACHE/SIG/kernel_close" ]]; then
-		echo "[*] Kernel close signal detected."
-		realEndOfSystem
-		break
-	else
-		pseudoEndOfSystem
-		clear
-		sleep 3
-	fi
-done
+beginningOfSystem
+cd "$ROOTFS"
+"$SYSTEM/sbin/interface"
+EOS
