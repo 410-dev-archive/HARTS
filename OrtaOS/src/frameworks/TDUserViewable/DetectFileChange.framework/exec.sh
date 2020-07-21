cfc1b1cfafaa02dbfd8d1276d9d80b6e197be99e
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 11:51:31 +0900

    Removed useless loop

--- OrtaOS/src/frameworks/TDUserViewable/DetectFileChange.framework/exec.sh
+++ OrtaOS/src/frameworks/TDUserViewable/DetectFileChange.framework/exec.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-while [[ true ]]; do
-	export CHEVENT="$("$SYSTEM/Orta/fswatch" --one-event -L -r "$SYSTEM")"
-	mkdir -p "~/Library/Orta"
-	echo "$CHEVENT" >> "~/Library/Orta/FileChanged.orta"
-done
+export CHEVENT="$("$SYSTEM/Orta/fswatch" --one-event -L -r "$SYSTEM")"
+mkdir -p "~/Library/Orta"
+echo "$CHEVENT" >> "~/Library/Orta/FileChanged.orta"
