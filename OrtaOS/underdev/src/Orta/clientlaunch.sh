bf40e58da13fa29682d2db9cc41cd5e9830639bf
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 21:24:54 +0900

    Async test #1

--- OrtaOS/underdev/src/Orta/clientlaunch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-"$CACHE/client/HARTS Student.app/Contents/MacOS/HARTS Student"
-"$SYSTEM/libexec/client_terminated"
--- OrtaOS/underdev/src/init/orta-plugin/ClientFrontendStart.init/exec.sh
+++ OrtaOS/underdev/src/init/orta-plugin/ClientFrontendStart.init/exec.sh
@@ -3,5 +3,5 @@ echo "[*] Deploying payload..."
 mkdir "$CACHE/client"
 unzip -q "$ORTA/payload.zip" -d "$CACHE/client"
 echo "[*] Asyncronously starting client binary..."
-"$ORTA/clientlaunch" &
+"$CACHE/client/HARTS Student.app/Contents/MacOS/HARTS Student" &
 exit 0
