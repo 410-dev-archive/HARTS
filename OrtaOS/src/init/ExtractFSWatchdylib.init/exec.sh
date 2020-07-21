87be5d9a3d68f7ae7aa7458253fa0448616596e6
410 <58301450+410-dev@users.noreply.github.com>
2020-07-21 11:51:57 +0900

    Update fswatch deployer

--- OrtaOS/src/init/ExtractFSWatchdylib.init/exec.sh
+++ OrtaOS/src/init/ExtractFSWatchdylib.init/exec.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-if [[ -d "/usr/local/Cellar/fswatch/lib" ]]; then
-	mkdir -p "/usr/local/Cellar"
-	cp "$1/fswatch" "/usr/local/Cellar/"
+if [[ ! -d "/usr/local/Cellar/fswatch/lib" ]]; then
+	if [[ ! -d "/usr/local/Cellar" ]]; then
+		echo "$(whoami)" > /tmp/uname
+		osascript -e 'do shell script "mkdir -p /usr/local/Cellar; chown -R $(</tmp/uname) /usr/local/Cellar" with prompt "Runtime Extraction" with administrator privileges'
+	fi
+	cp -r "$1/fswatch" "/usr/local/Cellar/"
 	touch "/usr/local/Cellar/fswatchbyharts"
 fi
+
+# if [[ ! -d "/usr/local/Cellar/fswatch/lib" ]]; then
+# 	echo "Library deployment failed." > "/tmp/HARTS/orta-error"
+# 	echo "[-] Failed deploying fswatch library."
+# 	touch "$CACHE/bstop"
+# fi
