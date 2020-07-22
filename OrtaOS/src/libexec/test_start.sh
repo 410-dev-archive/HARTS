2ea11ecdc468a1d85d70800f4f6450c5f05459ee
410 <58301450+410-dev@users.noreply.github.com>
2020-07-22 11:24:00 +0900

    Direct binary execute

--- OrtaOS/src/libexec/test_start.sh
+++ OrtaOS/src/libexec/test_start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-"$ORTA/launchdmgr-u"
-"$ORTA/locker"
+launchctl unload /System/Library/LaunchAgent/com.apple.Dock.agent.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.Finder.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.SystemUIServer.agent.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.quicklook.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.quicklook.ui.helper.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.Siri.agent.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.Spotlight.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.sidecar-relay.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.sharingd.plist
+launchctl unload /System/Library/LaunchAgent/com.apple.AirPlayUIAgent.plist
+
+killall Dock
+killall Finder
+killall SystemUIServer
+killall QuickLookUIService
+killall Siri
+killall Spotlight
+killall SidecarRelay
+killall sharingd
+killall AirPlayUIAgent
+killall ExternalQuickLookSatellite
