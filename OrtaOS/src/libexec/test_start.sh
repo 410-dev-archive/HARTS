d1c2b4263c80e41a8f8cb974a764314334ad818c
410 <58301450+410-dev@users.noreply.github.com>
2020-07-22 18:10:21 +0900

    Changed lockdown algorithm

--- OrtaOS/src/libexec/test_start.sh
+++ OrtaOS/src/libexec/test_start.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-launchctl unload /System/Library/LaunchAgent/com.apple.Dock.agent.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.Finder.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.SystemUIServer.agent.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.quicklook.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.quicklook.ui.helper.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.Siri.agent.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.Spotlight.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.sidecar-relay.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.sharingd.plist
-launchctl unload /System/Library/LaunchAgent/com.apple.AirPlayUIAgent.plist
-
-killall Dock
-killall Finder
-killall SystemUIServer
-killall QuickLookUIService
-killall Siri
-killall Spotlight
-killall SidecarRelay
-killall sharingd
-killall AirPlayUIAgent
-killall ExternalQuickLookSatellite
+killall -STOP Dock
+killall -STOP Finder
+killall -STOP SystemUIServer
+killall -STOP QuickLookUIService
+killall -STOP Siri
+killall -STOP Spotlight
+killall -STOP SidecarRelay
+killall -STOP sharingd
+killall -STOP AirPlayUIAgent
+killall -STOP ExternalQuickLookSatellite
