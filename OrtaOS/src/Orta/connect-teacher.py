8ce621526ef0363820764418d64a1638b41a4116
410 <58301450+410-dev@users.noreply.github.com>
2020-07-23 18:33:00 +0900

    placeholder

---
+++ OrtaOS/src/Orta/connect-teacher.py
@@ -0,0 +1,35 @@
+from socket import *
+import sys
+
+def send(sock, readUsername):
+	sock.send(readUsername.encode('utf-8'))
+
+def receive(sock):
+	recvData = sock.recv(1024).decode('utf-8')
+	if recvData == "REJECTED":
+		print("You are not accepted.")
+	elif recvData.startswith("ACCEPTED:"):
+		print("You are accepted.")
+		recvData = recvData.replace("ACCEPTED:", "")
+		Host = open("/tmp/HARTS/testhost.harts", "w")
+		Host.write(recvData)
+		Host.close()
+	else:
+		print("ERROR: Received unknown data: \"" + recvData + "\"")
+
+try:
+	if "." in sys.argv[1]:
+		f = 0 # Dummy code
+except:
+	exit(0)
+
+port = 27667
+print("[PY] Connecting Socket...")
+clientSock = socket(AF_INET, SOCK_STREAM)
+TeacherCOM = open("/tmp/HARTS/tip.harts", "r")
+clientSock.connect((TeacherCOM.read(), port))
+TeacherCOM.close()
+print("[PY] Sending packet: " + sys.argv[1])
+send(clientSock, sys.argv[1])
+receive(clientSock)
+
--- OrtaOS/src/libexec/commandkeyevent.sh
+++ OrtaOS/src/libexec/commandkeyevent.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 # $1 == 눌린 키값
+"$PYTHON" "$SYSTEM/Orta/teacherconnect.py" "report" "commandEvent=$1"
