d19744f734d286b1c74ed01e47d871970b03e1fb
ryubsmile <66876469+ryubsmile@users.noreply.github.com>
2020-07-24 00:18:15 +0900

    Key Reader Update
    added copy/paste message
    
    -crashes when capslock is pressed.

--- python-backend/KeyReader/KeystrokeReader.py
+++ python-backend/KeyReader/KeystrokeReader.py
@@ -1,28 +1,40 @@
 import keyboard
-import sendOrta
+import orta_ttyin
+
 
 def check_pressed_keys(a):
     for code in keyboard._pressed_events:
         line = str(code)
-        if line == "55":
-            print("L-cmd Pressed")
-            #sendOrta.OrtaOSPointer("lcmd")
+        if 0 <= int(line) <= 51:
+            continue
+        elif line == "55":
+            print("cmd Pressed")
+            #orta_ttyin.pushCommand("cmd")
+            if 8 in keyboard._pressed_events or 9 in keyboard._pressed_events: # cmd+c / cmd+v
+                print("No copy&pasting allowed.")
+                #orta_ttyin.pushCommand("copy/paste attempted")
         elif line == "61":
             print("L-option Pressed")
-            #sendOrta.OrtaOSPointer("loption")
+            #orta_ttyin.pushCommand("option")
         elif line == "58":
             print("R-option Pressed")
-            #sendOrta.OrtaOSPointer("roption")
+            #orta_ttyin.pushCommand("option")
         elif line == "56":
             print("L-shift Pressed")
-            #sendOrta.OrtaOSPointer("lshift")
+            #orta_ttyin.pushCommand("shift")
         elif line == "60":
             print("R-shift Pressed")
-            #sendOrta.OrtaOSPointer("rshift")
+            #orta_ttyin.pushCommand("shift")
         elif line == "59":
-            print("ctrl Pressed")
-            #sendOrta.OrtaOSPointer("ctrl")
-
-keyboard.hook(check_pressed_keys)
-keyboard.wait() # forever loop
+            print("L-ctrl Pressed")
+            #orta_ttyin.pushCommand("ctrl")
+        elif line == "62":
+            print("R-ctrl Pressed")
+            #orta_ttyin.pushCommand("ctrl")
+        elif line == "63":
+            print("fn Pressed")
+            #orta_ttyin.pushCommand("fn")
+        
+keyboard.on_press(check_pressed_keys)
+keyboard.wait()
 
