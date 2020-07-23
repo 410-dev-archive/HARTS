a6df685336592b45bbc18a0313b246538cd11383
410 <58301450+410-dev@users.noreply.github.com>
2020-07-23 16:33:17 +0900

    Extracts queue by eop helper

--- OrtaOS/src/init/orta-plugin/ExtractFSWatchdylib.init/exec.sh
+++ OrtaOS/src/init/orta-plugin/ExtractFSWatchdylib.init/exec.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 if [[ ! -d "/usr/local/Cellar/fswatch/lib" ]]; then
-	if [[ ! -d "/usr/local/Cellar" ]]; then
-		echo "$(whoami)" > /tmp/uname
-		osascript -e 'do shell script "mkdir -p /usr/local/Cellar; chown -R $(</tmp/uname) /usr/local/Cellar" with prompt "Runtime Extraction" with administrator privileges'
+	if [[ ! -d "/usr/local/Cellar/fswatch/lib" ]]; then
+		echo "$ASK_SUPERTOKEN" > "$ASK_SUPERUSER"
+		echo "mkdir -p /usr/local/Cellar" >> "$CACHE/superlist"
+		echo "chown -R $(whoami) /usr/local/Cellar" >> "$CACHE/superlist"
+		echo "mkdir -p /usr/local/Cellar/fswatch" >> "$CACHE/superlist"
+		echo "unzip -q \"$1/fswatch.zip\" -d \"/usr/local/Cellar\"" >> "$CACHE/superlist"
+		echo "rm -rf \"/usr/local/Cellar/__MACOSX\"" >> "$CACHE/superlist"
+		echo "touch \"/usr/local/Cellar/fswatchbyharts\"" >> "$CACHE/superlist"
 	fi
-	mkdir -p "/usr/local/Cellar/fswatch"
-	unzip -q "$1/fswatch.zip" -d "/usr/local/Cellar"
-	rm -rf "/usr/local/Cellar/__MACOSX"
-	touch "/usr/local/Cellar/fswatchbyharts"
 fi
