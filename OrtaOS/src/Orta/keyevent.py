2abd76d8dad276a50d7adca80fa3f91e7d147aef
410 <58301450+410-dev@users.noreply.github.com>
2020-07-24 15:47:41 +0900

    Temporary scripts

---
+++ OrtaOS/src/Orta/keyevent.py
@@ -0,0 +1,27 @@
+import time
+from pynput.keyboard import Key, Listener # $ pip3 install pynput
+import orta_ttyin
+
+def on_press(key):
+    """
+    e.g. len('X') == 3, thus any single character including alphabet or number is eliminated
+    from the following commands.
+    """
+    if len(str(key)) > 3:
+        msg = ('{0} DOWN'.format(key))
+        msg = msg.replace("Key.","")
+        print(msg)
+        #orta_ttyin.pushCommand(msg) #send to ortaOS
+        
+def on_release(key):
+    #print('{0} release'.format(key))
+    if key == Key.esc:
+        """
+        purpose: Stopping listener,
+        erase for actual use to infinite loop until taskkilled. = daemon thread.
+        """
+        return False
+
+# Collect events until esc released
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
