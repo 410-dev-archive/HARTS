5320fda90dfda0a646a399578d83c2dd51939a24
ryubsmile <66876469+ryubsmile@users.noreply.github.com>
2020-07-24 18:31:47 +0900

    cmd detection added to keyreader
    alphabet or number = dismiss
    
    cmd + c,x,v = copy/paste
    cmd + w,q = forcequit
    other modifier = only key DOWN
    
    all printed messages goes to OrtaOS as well.
    
    esc to cancel for now.

--- python-backend/KeyReader/key_reader.py
+++ python-backend/KeyReader/key_reader.py
@@ -4,26 +4,40 @@ sys.dont_write_bytecode = True
 from pynput.keyboard import Key, Listener # $ pip3 install pynput
 import orta_ttyin
 
+cmd_pressed = False # is command pressed? initial value
+
+def toggle_cmd_pressed(): # cmd DOWN -> true, cmd UP -> false
+    global cmd_pressed
+    cmd_pressed = not cmd_pressed
+
 def on_press(key):
-    """
-    e.g. len('X') == 3, thus any single character including alphabet or number is eliminated
-    from the following commands.
-    """
-    if len(str(key)) > 3:
-        msg = ('{0} DOWN'.format(key))
-        msg = msg.replace("Key.","")
+    if len(str(key)) > 3: # dismisses 1-letter key e.g. = 'p'
+        msg = ('{0} DOWN'.format(key)).replace("Key.","") # format = button UP
         print(msg)
         #orta_ttyin.pushCommand(msg) #send to ortaOS
-        
+    if key in {Key.cmd,Key.cmd_l,Key.cmd_r}:
+        toggle_cmd_pressed()
+    elif cmd_pressed == True:
+        if str(key) in {"'c'","'x'","'v'"}:
+            print("Copy/Pasting Attempted.") 
+            #orta_ttyin.pushCommand("commandkeyevent Copy&Paste")
+        elif str(key) in {"'w'","'q'"}:
+            print("Forceful Quit Attempted.") 
+            #orta_ttyin.pushCommand("commandkeyevent ForceQuit")
+
 def on_release(key):
-    #print('{0} release'.format(key))
+    if key in {Key.cmd,Key.cmd_l,Key.cmd_r}: # only sends cmd UP
+        toggle_cmd_pressed()
+        msg = ('{0} UP'.format(key)).replace("Key.","")
+        print(msg)
+        #orta_ttyin.pushCommand(msg)
     if key == Key.esc:
         """
-        purpose: Stopping listener,
-        erase for actual use to infinite loop until taskkilled. = daemon thread.
+        purpose: Stopping listener.
+        ERASE for actual use to loop until taskkilled. = daemon thread.
         """
         return False
 
-# Collect events until esc released
+# Collect events until on_release returns False. For infinite loop - on_release=True
 with Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
