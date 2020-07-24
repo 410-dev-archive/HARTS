1af7423d578b4c0750a35c3aad7b7ff051389429
ryubsmile <66876469+ryubsmile@users.noreply.github.com>
2020-07-24 18:50:15 +0900

    added quit condition to reader

--- python-backend/KeyReader/key_reader.py
+++ python-backend/KeyReader/key_reader.py
@@ -2,7 +2,7 @@ import sys # cache disable
 sys.dont_write_bytecode = True
 
 from pynput.keyboard import Key, Listener # $ pip3 install pynput
-import orta_ttyin
+import orta_ttyin, os
 
 cmd_pressed = False # is command pressed? initial value
 
@@ -13,31 +13,35 @@ def toggle_cmd_pressed(): # cmd DOWN -> true, cmd UP -> false
 def on_press(key):
     if len(str(key)) > 3: # dismisses 1-letter key e.g. = 'p'
         msg = ('{0} DOWN'.format(key)).replace("Key.","") # format = button UP
-        print(msg)
-        #orta_ttyin.pushCommand(msg) #send to ortaOS
+        print("keyevent", msg)
+        #orta_ttyin.pushCommand("keyevent"+msg) #send to ortaOS
     if key in {Key.cmd,Key.cmd_l,Key.cmd_r}:
         toggle_cmd_pressed()
     elif cmd_pressed == True:
         if str(key) in {"'c'","'x'","'v'"}:
-            print("Copy/Pasting Attempted.") 
+            print("commandkeyevent Copy&Paste") 
             #orta_ttyin.pushCommand("commandkeyevent Copy&Paste")
         elif str(key) in {"'w'","'q'"}:
-            print("Forceful Quit Attempted.") 
+            print("commandkeyevent ForceQuit") 
             #orta_ttyin.pushCommand("commandkeyevent ForceQuit")
 
 def on_release(key):
     if key in {Key.cmd,Key.cmd_l,Key.cmd_r}: # only sends cmd UP
         toggle_cmd_pressed()
         msg = ('{0} UP'.format(key)).replace("Key.","")
-        print(msg)
-        #orta_ttyin.pushCommand(msg)
+        print("keyevent",msg)
+        #orta_ttyin.pushCommand("keyevent"+msg)
     if key == Key.esc:
         """
         purpose: Stopping listener.
-        ERASE for actual use to loop until taskkilled. = daemon thread.
+        change condition to 
+        
+        not os.path.exists('/tmp/HARTS/ortaos/vrootfs/System') 
+
+        for actual use.
         """
         return False
 
-# Collect events until on_release returns False. For infinite loop - on_release=True
+# Collect events until on_release returns False.
 with Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
