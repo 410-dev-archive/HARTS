87784e46832fb97be89b8966d0341a779b628660
410 <58301450+410-dev@users.noreply.github.com>
2020-07-24 09:23:10 +0900

    Merged arguments

--- OrtaOS/src/libexec/sendpacket.sh
+++ OrtaOS/src/libexec/sendpacket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$1" == "teacher" ]]; then
-	"$PYTHON" "$ORTA/server-host.py" "$2" "$3" "$4" "$5" "$6"
+	"$PYTHON" "$ORTA/server-host.py" "$2 $3 $4 $5 $6 $7 $8 $9"
 elif [[ "$1" == "master" ]]; then
-	"$PYTHON" "$ORTA/server-master.py" "$2" "$3" "$4" "$5" "$6"
+	"$PYTHON" "$ORTA/server-master.py" "$2 $3 $4 $5 $6 $7 $8 $9"
 fi
