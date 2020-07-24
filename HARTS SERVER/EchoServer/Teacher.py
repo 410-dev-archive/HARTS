d6865381800846dc1b1ff241f66cc9404bef4b50
410 <58301450+410-dev@users.noreply.github.com>
2020-07-24 09:20:58 +0900

    More acceptable commands for teacher

--- HARTS SERVER/EchoServer/Teacher.py
+++ HARTS SERVER/EchoServer/Teacher.py
@@ -5,25 +5,49 @@ TEST_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSd7kImJ6H3wqdHWYEssvSnDacK
 
 def send(sock, message):
 	sock.send(message.encode('utf-8'))
+	print("[*] Returned: " + message)
 
 def receive(sock):
 	try:
 		recvData = sock.recv(1024).decode('utf-8')
-		if ReadStudentNameList.index(recvData.split(":")[1]) >= 0:
-			if recvData.split(":")[0] == "JOIN":
+		if recvData.startswith("JOIN"):
+
+			# On Join signal
+			# JOIN:NAME
+			if recvData.split(":")[1] in ReadStudentNameList: 
 				send(sock, "ACCEPTED:" + TEST_LINK)
 				print(recvData.split(":")[1] + " joined the session.")
 			else:
-				print(recvData.split(":")[1] + " left the session.")
-		else:
-			send(sock, "REJECTED")
-			print(recvData + " tried to join the session, but was rejected.")
-	except ValueError:
-		send(sock, "REJECTED")
-		print(recvData + " tried to join the session, but was rejected.")
+				send(sock, "REJECTED")
+				print(recvData + " tried to join the session, but was rejected.")
+
+		elif recvData.startswith("LEAVE"):
+
+			# On leave signal
+			# LEAVE:NAME
+			print(recvData.split(":")[1] + " left the session.")
+			send(sock, "OK")
+
+		elif recvData.startswith("QUESTION"):
+
+			# On Question signal
+			# QUESTION:NAME:CONTENTS
+			print(recvData.split(":")[1] + " asked question: " + recvData.split(":")[2])
+			send(sock, "OK")
+
+		elif recvData.startswith("KEYEVENT"):
+
+			# On keyboard signal
+			# KEYEVENT:NAME:PRESSED KEY
+			print("Detected " + recvData.split(":")[1] + "\'s suspicious keystroke: " + recvData.split(":")[2])
+			send(sock, "OK")
+
+		else
+			print("Received unknown data: " + recvData)
+
 	except:
 		send(sock, "ERROR")
-		print(recvData + " tried to join the session, but the server encountered an error.")
+		print("Received " + recvData + "\" but the server encountered an error.")
 
 port = 27667
 serverSock = socket(AF_INET, SOCK_STREAM)
