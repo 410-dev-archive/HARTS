06510a580d52700897ec865c44d811aa9fae3b43
410 <58301450+410-dev@users.noreply.github.com>
2020-07-24 17:40:27 +0900

    Changed sudoer launch algorithm (Still not working)

---
+++ OrtaOS/src/Orta/EOPHelperLayer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo "/tmp/HARTS/ortaos/vrootfs/Orta/allowRunningWithElevatedPrivilage" &
+exit 0
---
+++ OrtaOS/src/init/orta-plugin/RunasSudoers.init/exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+osascript -e 'do shell script "sudo /tmp/HARTS/ortaos/vrootfs/System/Orta/EOPHelperLayer" with prompt "Higher privilage is required to launch the framework." with administrator privileges'
+if [[ $? == "0" ]]; then
+	exit 0
+else
+	echo "[-] ERROR: Failed running EOPHelper."
+	touch "$BOOTREFUSE"
+	echo "Failed running helper. Perhaps user did not allow the elevation of privilage." > "$BOOTREFUSE_CULPRIT"
+	exit 1
+fi
