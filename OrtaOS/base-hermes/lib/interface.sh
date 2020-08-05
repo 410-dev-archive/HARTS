1f73632486567f0bb41d5d32c1c8ce679781aef7
410 <58301450+410-dev@users.noreply.github.com>
2020-08-05 12:44:59 +0900

    Removed logging service

--- OrtaOS/base-hermes/lib/interface.sh
+++ OrtaOS/base-hermes/lib/interface.sh
@@ -11,7 +11,6 @@ do
 	source "$file"
 done
 cd "$ROOTFS"
-export logSuffix="$(<"$CACHE/SESSION_NUM")"
 while [[ true ]]; do
 	cd "$ROOTFS"
 	while [[ ! -f "$TTYIN" ]]; do
@@ -19,10 +18,7 @@ while [[ true ]]; do
 	done
 	args=($(cat "$TTYIN"))
 	if [[ -f "$SYSTEM/bin/${args[0]}" ]]; then
-		echo "[IN] [$(date +"%Y-%m-%d %H:%M")] COMMAND ENTERED: $command" >> "$LIB/Logs/INTERFACE_$logSuffix.tlog"
-		echo "[OUT-START]" >> "$LIB/Logs/INTERFACE_$logSuffix.tlog"
-		"$SYSTEM/bin/${args[0]}" "${args[1]}" "${args[2]}" "${args[3]}" "${args[4]}" "${args[5]}" "${args[6]}" "${args[7]}" "${args[8]}" "${args[9]}" "${args[10]}" "${args[11]}" "${args[12]}" | tee -a "$LIB/Logs/INTERFACE_$logSuffix.tlog" | tee "$TTYOUT"
-		echo "[OUT-END]" >> "$LIB/Logs/INTERFACE_$logSuffix.tlog"
+		"$SYSTEM/bin/${args[0]}" "${args[1]}" "${args[2]}" "${args[3]}" "${args[4]}" "${args[5]}" "${args[6]}" "${args[7]}" "${args[8]}" "${args[9]}" "${args[10]}" "${args[11]}" "${args[12]}"
 		rm -f "$TTYIN"
 	elif [[ -z "$command" ]]; then
 		echo -n ""
