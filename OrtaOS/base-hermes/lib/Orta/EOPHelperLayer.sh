e8ae38ca5f8f00fc1406798523d59ec7dcc353c4
410 <58301450+410-dev@users.noreply.github.com>
2020-08-05 15:29:37 +0900

    Bug fix..?

--- OrtaOS/base-hermes/lib/Orta/EOPHelperLayer.sh
+++ OrtaOS/base-hermes/lib/Orta/EOPHelperLayer.sh
@@ -3,16 +3,20 @@ source "/tmp/HARTS/ortaos/vrootfs/System/boot/PLT"
 
 # Extract FSWatch library
 if [[ ! -d "/usr/local/Cellar/fswatch" ]]; then
+	echo "Extracting fswatch"
 	mkdir -p "/usr/local/Cellar"
 	unzip -q "$ORTA/fswatch.zip" -d "/usr/local/Cellar"
 	rm -rf "/usr/local/Cellar/__MACOSX"
+	chown -R "$(<"$CACHE/username")" "/usr/local/Cellar/fswatch"
 fi
 
 # Extract Python
 if [[ ! -d "/usr/local/Cellar/python@3.8" ]]; then
+	echo "Extracting python"
 	mkdir -p "/usr/local/Cellar"
 	unzip -q "$ORTA/python-wpip.zip" -d "/usr/local/Cellar"
 	rm -rf "/usr/local/Cellar/__MACOSX"
+	chown -R "$(<"$CACHE/username")" "/usr/local/Cellar/python@3.8"
 fi
 
 exit 0
